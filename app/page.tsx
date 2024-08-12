b19f7b5597a88c6e956133be448c444f86d2e979
AlexMoreni <alex.moreni47@gmail.com>
2024-08-12 14:10:41 -0300

    feat: add BarbershopItem component

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,12 @@ import { Input } from "@/components/ui/input"
 import { SearchIcon } from "lucide-react"
 import Image from "next/image"
 
-export default function Home() {
+import { db } from "@/lib/prisma"
+import BarbershopItem from "@/components/barbershop-item"
+
+export default async function Home() {
+  const barbershops = await db.barbershop.findMany({})
+
   return (
     <>
       <Header />
@@ -35,7 +40,10 @@ export default function Home() {
         </div>
 
         {/* AGENDAMENTO */}
-        <Card className="mt-6">
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+        <Card>
           <CardContent className="flex justify-between p-0">
             <div className="flex flex-col gap-2 py-5 pl-5">
               <Badge className="w-fit">Confirmado</Badge>
@@ -54,6 +62,16 @@ export default function Home() {
             </div>
           </CardContent>
         </Card>
+
+        {/* RECOMENDADOS */}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+        <div className="[&::-webkit-scrollbar-thumb] flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
     </>
   )
