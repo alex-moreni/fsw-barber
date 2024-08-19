c90e3c4a5704ca785e4c102a370aa4994318d74c
AlexMoreni <alex.moreni47@gmail.com>
2024-08-19 15:01:58 -0300

    feat: ensure only authenticated user can book

--- app/_actions/create-booking.ts
+++ app/_actions/create-booking.ts
@@ -1,17 +1,24 @@
 "use server"
 
+import { authOptions } from "@/lib/auth"
 import { db } from "@/lib/prisma"
+import { getServerSession } from "next-auth"
 import { revalidatePath } from "next/cache"
 
 interface CreateBookingParams {
-  userId: string
   serviceId: string
   date: Date
 }
 
 export const createBooking = async (params: CreateBookingParams) => {
+  const user = await getServerSession(authOptions)
+
+  if (!user) {
+    throw new Error("User not found")
+  }
+
   await db.booking.create({
-    data: params,
+    data: { ...params, userId: (user.user as any).id },
   })
   revalidatePath("/barbershops/[id]")
 }
