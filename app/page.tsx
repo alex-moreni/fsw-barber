4bf73e06d686b00a9475b2e317e0e547233a4181
AlexMoreni <alex.moreni47@gmail.com>
2024-08-23 13:38:02 -0300

    feat: update welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,8 @@ import Search from "@/components/search"
 import Link from "next/link"
 import { getServerSession } from "next-auth"
 import { authOptions } from "@/lib/auth"
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale"
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
@@ -46,8 +48,18 @@ export default async function Home() {
       <Header />
       <div className="p-5">
         {/* TEXTO */}
-        <h2 className="text-xl font-bold">Olá, Fulano!</h2>
-        <p>Segunda-feira, 12 de agosto.</p>
+        <h2 className="text-xl font-bold">
+          Olá, {session?.user ? session.user.name : "Bem-vindo"}!
+        </h2>
+        <p>
+          <span className="capitalize">
+            {format(new Date(), "EEEE, dd", { locale: ptBR })}
+          </span>
+          <span>&nbsp;de&nbsp;</span>
+          <span className="capitalize">
+            {format(new Date(), "MMMM", { locale: ptBR })}
+          </span>
+        </p>
 
         {/* BUSCA */}
         <div className="mt-6">
